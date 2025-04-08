5ece4c5adc0952ea587b0acffd3c5c86df4e2987
Harshitha <23wh1a0598@bvrithyderabad.edu.in>
2025-04-08 10:29:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import TodoForm from './components/todoform';
+import TodoList from './components/todolist';
+function App() {
+const [todos, setTodos] = useState([]);
+const addTodo = (text) => {
+setTodos([...todos, { id: Date.now(), text }]);
+};
+const deleteTodo = (id) => {
+setTodos(todos.filter((todo) => todo.id !== id));
+};
+return (
+<div className="App">
+<h1>TODO App</h1>
+<TodoForm addTodo={addTodo} />
+<TodoList todos={todos} deleteTodo={deleteTodo} />
+</div>
+);
+}
+export default App;
